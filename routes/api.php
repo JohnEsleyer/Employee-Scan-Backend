c61c5314a092abdd36d16ab28310df5beb9aa936
john <john@pop-os.localdomain>
2023-07-03 14:52:48 +0800

    fixed valid token not being accepted

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AttendanceController;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,28 +16,34 @@ use App\Http\Controllers\AttendanceController;
 |
 */
 
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
 
 Route::get('/hello', function(){
     return response()->json(['message' => 'Hello, API!']);
 });
 
-// Employee Endpoints
-Route::get('/employee', [EmployeeController::class, 'index']);
-Route::get('/employee/{id}', [EmployeeController::class, 'show']);
-Route::post('/employee', [EmployeeController::class, 'store']);
-Route::put('/employee/{id}', [EmployeeController::class, 'update']);
-Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
-Route::get('/employee/{id}/exists', [EmployeeController::class, 'exists']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Employee Endpoints
+    Route::get('/employee', [EmployeeController::class, 'index']);
+    Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+    Route::post('/employee', [EmployeeController::class, 'store']);
+    Route::put('/employee/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
+    Route::get('/employee/{id}/exists', [EmployeeController::class, 'exists']);
 
-
-// Attendance Endpoints
-Route::post('/attendance', [AttendanceController::class, 'store']);
-Route::delete('/attendance/{id}', [AttendanceController::class, 'destroy']);
-Route::get('/attendance/{id}', [AttendanceController::class, 'show']);
-Route::get('/attendance', [AttendanceController::class, 'index']);
-Route::get('/attendance/{id}/exists', [AttendanceController::class, 'exists']);
-
-
+    // Attendance Endpoints
+    Route::post('/attendance', [AttendanceController::class, 'store']);
+    Route::delete('/attendance/{id}', [AttendanceController::class, 'destroy']);
+    Route::get('/attendance/{id}', [AttendanceController::class, 'show']);
+    Route::get('/attendance', [AttendanceController::class, 'index']);
+    Route::get('/attendance/{id}/exists', [AttendanceController::class, 'exists']);
+});
