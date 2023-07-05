5deddd67b8ef6898216f2477d7e3601b23859a4e
john <john@pop-os.localdomain>
2023-07-05 14:16:16 +0800

    added login view for web

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Your Application') }}</title>
+    <!-- Include your CSS stylesheets and JavaScript files here -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <!-- Your navigation bar or header section -->
+    <header>
+        <!-- Place your logo, navigation links, etc. here -->
+    </header>
+
+    <!-- The main content of each page -->
+    <main>
+        @yield('content')
+    </main>
+
+    <!-- Your footer section -->
+    <footer>
+        <!-- Place your footer content here -->
+    </footer>
+
+    <!-- Include your JavaScript files here -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
